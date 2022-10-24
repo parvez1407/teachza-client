7992e31c44b4ae47a22abd131efe5399d0457d51
parvez1407 <info.nahidwd@gmail.com>
2022-10-25 00:35:48 +0600

    create an app and all installation done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDX75EybreAAgY2MFkZloKdsS03wNobcjA",
+    authDomain: "teachza-e0177.firebaseapp.com",
+    projectId: "teachza-e0177",
+    storageBucket: "teachza-e0177.appspot.com",
+    messagingSenderId: "197860222611",
+    appId: "1:197860222611:web:43f9cc917260ff4cdd0f66"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
