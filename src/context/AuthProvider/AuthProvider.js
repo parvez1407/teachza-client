b903bdfa33becfa5f18ce56585974bbe7d1af41d
parvez1407 <info.nahidwd@gmail.com>
2022-10-26 02:15:07 +0600

    Decalred auth provider and share with all component

--- src/context/AuthProvider/AuthProvider.js
+++ src/context/AuthProvider/AuthProvider.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { createContext } from 'react';
-import getAuth from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
 import app from '../../firebase/firebase.config';
+import { useState } from 'react';
 
 export const AuthContext = createContext();
 
 const auth = getAuth(app);
 
-const AuthProvider = () => {
-    return (
-        <AuthContext.Provider>
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
 
+
+
+    const authInfo = {
+        user
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
         </AuthContext.Provider>
     );
 };
--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AuthProvider from './context/AuthProvider/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
